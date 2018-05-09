f022cee57e6d6f9a31d39678cedead206d27c32e
true_blue <trojanhorse23pub@gmail.com>
2018-05-09 21:29:35 +0530

    7

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-<?php include_once(“index.html”); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Heart Game</title>
+    <!-- <link rel="stylesheet" href="https://bootswatch.com/4/cyborg/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="https://bootswatch.com/4/slate/bootstrap.min.css">
+    <style>
+    html, body {
+    height: 100%;
+}
+
+html {
+    display: table;
+    margin: auto;
+}
+
+body {
+    display: table-cell;
+    vertical-align: middle;
+}
+    </style>
+  </head>
+  <body>
+<input type="text" id="gfname">
+<button id="btn">See who is in my heart</button>
+<script src="https://code.jquery.com/jquery-1.9.1.js"></script>
+<script src="main.js"></script>
+</body>
+</html>
